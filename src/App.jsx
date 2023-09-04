63c41af848d8ff9715fa3bddf013a7f7907246e7
Adi <adithiyans123@gmail.com>
2023-09-04 08:40:05 +0530

    setup giphy api

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,25 @@ import './App.css'
 import { useEffect } from 'react'
 import { useState } from 'react'
 
+const key = import.meta.env.VITE_GIPHY_KEY;
+
 function App() {
   
-  let [links,setLinks] = useState({});
+  let [links,setLinks] = useState();
 
   useEffect(()=>{
     
-      setLinks({...links,['ed'] :'ex'});
-      console.log("finsihd");
+      async function foo(){
+
+        let linkarray = [];
+        let li = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${key}&tag=&rating=g`,{mode:"cors"});
+        let lj = await li.json();
+        console.log(lj.data.images.downsized.url);
+
+      }
+
+      foo();
+
 
   },[]);
 
