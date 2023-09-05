1eba997f986630693f5c782e1ccc14629f0890b8
Adi <adithiyans123@gmail.com>
2023-09-05 10:16:22 +0530

    stored pokes in state

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { useState } from 'react'
 import { Pokedex } from 'pokeapi-js-wrapper'
 
 
-const key = import.meta.env.VITE_GIPHY_KEY;
 //pokemopn api stuff
 
 const P = new Pokedex;
@@ -15,28 +14,61 @@ const P = new Pokedex;
 
 function App() {
   
+
+  //links used to store stuff about pokemon and their images
   let [links,setLinks] = useState();
 
+
+  //runs on mount to make api cALLS and store stuff in state
   useEffect(()=>{
     
-      async function foo(){
-        let pokemon = ['pikachu','squirtle','charmander','bulbasaur'];
+      async function getPokeDetails(){
+        let pokemon = [
+          "pikachu",
+          "charizard",
+          "bulbasaur",
+          "squirtle",
+          "jigglypuff",
+          "mewtwo",
+          "gengar",
+          "snorlax",
+          "eevee",
+          "machamp",
+          "gyarados",
+          "alakazam",
+          "vaporeon",
+          "dragonite",
+          "golem",
+          "lapras",
+          "mew",
+          "pidgeot",
+          "raichu",
+          "blastoise"
+        ];
+
+
         let linkobj = {};
         //to get gif
-        for(let i=0;i<4;i++){
+        for(let i=0;i<19;i++){
         //let li = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${pokemon[i]}`,{mode:"cors"});
         //let lj = await li.json();
         //console.log(lj.data.images.original.url);
+
+
+
         //to get description and photos
 
-        
-          let x = await P.getPokemonByName(pokemon[i]);
+        //gets poke detAILS
+        let x = await P.getPokemonByName(pokemon[i]);
           
-
+          //stores in linkobj
           linkobj = {...linkobj, [i] : {'name': x.name,'abilities' : x.abilities,'url': x.sprites.front_default}};
-
+          console.log(x.sprites.front_default);
        
         }
+
+
+        //stores stuff in state
 
         setLinks(linkobj)
 
@@ -44,7 +76,7 @@ function App() {
 
       }
 
-      foo();
+      getPokeDetails();
 
 
   },[]);
