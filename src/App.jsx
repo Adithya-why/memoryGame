7cb6c36e7b8b4c4827e8cb3ade6a931c5a3eca53
Adi <adithiyans123@gmail.com>
2023-09-07 15:49:58 +0530

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -7,55 +7,66 @@ import { useState } from 'react'
 
 import { Pokedex } from 'pokeapi-js-wrapper'
 
+
+//key of giphy api from env file
 const key = import.meta.env.VITE_GIPHY_KEY;
 //pokemopn api stuff
-
 const P = new Pokedex;
 
 
 function App() {
+
+  //array to hold list of pokemon
+  let [pokemon,setpokemon] = useState([
+    "pikachu",
+    "charizard",
+    "bulbasaur",
+    "squirtle",
+    "jigglypuff",
+    "mewtwo",
+    "gengar",
+    "snorlax",
+    "eevee",
+    "machamp",
+    "gyarados",
+    "alakazam",
+    "vaporeon",
+    "dragonite",
+    "golem",
+    "lapras",
+    "mew",
+    "pidgeot",
+    "raichu",
+    "blastoise"
+  ]);
+  
   
 
   //links used to store stuff about pokemon and their images
   let [links,setLinks] = useState();
 
-  //array to store the card structures
+  
   
 
   //runs on mount to make api cALLS and store stuff in state
+
   useEffect(()=>{
+    console.log('effect running')
     
+
+    //obj to store details
+      let linkobj = {};
+
       async function getPokeDetails(){
-        let pokemon = [
-          "pikachu",
-          "charizard",
-          "bulbasaur",
-          "squirtle",
-          "jigglypuff",
-          "mewtwo",
-          "gengar",
-          "snorlax",
-          "eevee",
-          "machamp",
-          "gyarados",
-          "alakazam",
-          "vaporeon",
-          "dragonite",
-          "golem",
-          "lapras",
-          "mew",
-          "pidgeot",
-          "raichu",
-          "blastoise"
-        ];
+        
+        
 
-        //obj to store details
-
-        let linkobj = {};
-
+        
 
         //to get gif
         for(let i=0;i<20;i++){
+
+          //gets stuff from giphy
         let li = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${pokemon[i]}`,{mode:"cors"});
         let lj = await li.json();
         let alt = lj.data.images.original.url;
@@ -63,7 +74,7 @@ function App() {
 
 
         //to get description and photos
-
+          //gets stuff from pokeapi
         //gets poke detAILS
         let x = await P.getPokemonByName(pokemon[i]);
           
@@ -89,15 +100,17 @@ function App() {
       getPokeDetails();
 
 
-  },[]);
+  },[pokemon]);
 
 
 //create the cards
 let picar = [];
+
 if(links){
   for(let i=0;i<19;i++){
     picar.push(<PictureCard details={links[i]} key={i}/>)
   }
+ 
 }
   
 
@@ -114,9 +127,16 @@ if(links){
     <h1>Pictures</h1>
 
     {/*COmditional rendering*/ }
-    <div>
+    <div className='cards'>
       {links ? (picar) : <p>Loading........</p>}
     </div>
+
+    <button onClick={()=>{
+      let temp = pokemon;
+      temp.reverse();
+      console.log('hi');
+      setpokemon([...temp]);
+    }}>Shuffle</button>
     
     </>
   )
--- src/components/PictureCard.jsx
+++ src/components/PictureCard.jsx
@@ -3,14 +3,14 @@ export default function PictureCard(props){
     let name = props.details.name;
 
     return(
-        <>
+        <div className="card">
 
 
         <div>CARD </div>
         <div>{name}</div>
-        <img src={props.details.alt}/>
+        <img src={props.details.alt} width={100} height={100} />
 
-        </>
+        </div>
     )
 
 }
